42073f4bacbd3a9ef6e25bc40762f0d9559e567e
Tryd0g0lik <work80@mail.ru>
2025-05-14 11:38:44 +0700

    [dev]: SH-files was changed

--- pull.sh
+++ pull.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 cd /home/denis/adboard || exit 1
 git pull --ff-only origin dev  || exit 2
+expect {
+   "password" { send -- "123\r";}  # Ждёт новые "password"  exp_continue
+   "sudo"     { send -- "123\r"; exp_continue }  # Или "sudo"
+   eof  # Выход при завершении
+}
 # git commit -m "Update" || exit 1
 sudo systemctl restart gunicorn  || exit 5
 sudo systemctl restart nginx  || exit 4
