8cc796829a2b8a6a37504db9c9d710a27f49bd77
Tryd0g0lik <work80@mail.ru>
2025-05-14 08:03:56 +0700

    [dev]: first commit

---
+++ pull.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd /home/denis/adboard || exit 1
+git pull --ff-only origen adboard  || exit 1
+#git pull origin tests  || exit 1
+#git fetch origin tests
+# git reset --hard origin/test
+#git checkout remotes/origin/tests
+#git add *
+#git checkout tests
+#git merge --no-ff --no-edit remotes/origin/tests
+git add * || exit 1
+git commit -m "Update" || exit 1
+sudo systemctl restart gunicorn  || exit 1
+echo "Deployment completed successfully"
+
