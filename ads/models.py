bc1b3a0ad40d4dce5ad43689679bfc7a749c1c5b
Tryd0g0lik <work80@mail.ru>
2025-05-14 10:52:12 +0700

    [dev]: test commit

--- ads/models.py
+++ ads/models.py
@@ -1,10 +1,17 @@
-from django.db import models
+"""
+ads/models.py
+"""
+
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
 # from django.utils.translation import get_language as _
 # Create your models here.
 
+
 class CustomUser(AbstractUser):
     pass
+
     class Meta:
         # Add this if you haven't already
-        swappable = 'AUTH_USER_MODEL'
+        swappable = "AUTH_USER_MODEL"
