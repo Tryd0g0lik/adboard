f25ccfd5fa8e887911a54466264613dda68e1471
Tryd0g0lik <work80@mail.ru>
2025-05-14 08:28:25 +0700

    [dev]: models was update

--- ads/admin.py
+++ ads/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.models import AbstractUser
+
 
 # Register your models here.
+class CustomUser(AbstractUser):
+    class Meta:
+        abstract = True
