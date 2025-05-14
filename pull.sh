0ce7cb06643792ae7cf37a6fb948f6afe29b1fed
Tryd0g0lik <work80@mail.ru>
2025-05-14 11:22:41 +0700

    [dev]: SH-files was changed

--- pull.sh
+++ pull.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 cd /home/denis/adboard || exit 1
 git pull --ff-only origin dev  || exit 2
-#git pull origin tests  || exit 1
-#git fetch origin tests
-# git reset --hard origin/test
-#git checkout remotes/origin/tests
-#git add *
-#git checkout tests
-#git merge --no-ff --no-edit remotes/origin/tests
-git add * || exit 3
 # git commit -m "Update" || exit 1
 sudo systemctl restart gunicorn  || exit 5
-sudo systemctl restart nginx  || exit 6
+sudo systemctl restart nginx  || exit 4
 echo "Deployment completed successfully"
+#git fetch origin tests
+# git reset --hard origin/test
 
