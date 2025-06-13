1ed8db0309636a0264a55851ebb30f555dd41811
Tryd0g0lik <work80@mail.ru>
2025-06-13 16:24:00 +0700

    [tests]  display - got async test for user's register from form. got fixture for cleaning db

--- __tests__/__fixtures__/playwrigth_fixture.py
+++ __tests__/__fixtures__/playwrigth_fixture.py
@@ -2,94 +2,18 @@ import asyncio
 import logging
 import os
 import pytest
-from asgiref.sync import sync_to_async
 from playwright.sync_api import Playwright, sync_playwright
+from playwright.async_api import Playwright, async_playwright
 from project import settings
 from django.contrib.auth.models import User
 from dotenv import load_dotenv
+from logs import configure_logging
 
 load_dotenv()
+log = logging.getLogger(__name__)
+configure_logging(logging.INFO)
 
 
-@pytest.fixture(
-    autouse=True,
-)
-def browser():
-    """
-    This function will be used for launching browser
-    :param playwright:
-    :return:
-    """
-    logging.info("START BROWSER FIXTURE")
-    playwright = sync_playwright()
-    chromium = playwright.start().chromium
-    logging.info("RECEIVED CHROMIUM")
-    browser = chromium.launch()
-    logging.info("START CONTEXT")
-    return browser
-
-
-@pytest.fixture(autouse=True)
-def django_db_setup():
-    settings.DATABASES["default"]["TEST"] = {
-        "ENGINE": os.getenv("ENGINE"),
-        "NAME": os.getenv("POSTGRES_DB"),
-        "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("POSTGRES_HOST"),
-        "PORT": os.getenv("POSTGRES_PORT"),
-    }
-
-
-#
-# @pytest.fixture(autouse=True)
-# def check_user(django_db_blocker, django_db_setup):
-# def page():
-# yield
-# with django_db_blocker.unblock():
-#     """CHECK OF USER IN DB"""
-#     logging.info("START CHECK OF USER IN DB")
-#     django_db_blocker.unblock()
-#     # django_db_blocker.unblock():
-#     logging.info("1")
-#
-#     count = sync_to_async(User.objects.count())
-#     logging.info("COUNT USER", count, count == 1)
-#     assert count == 1
-#     user = sync_to_async(User.objects.filter(username="Sergey"))
-#     assert sync_to_async(user.exists())
-#     logging.info("USER NAME", user[0].username)
-#     sync_to_async(user[0].delete())
-#     sync_to_async(user[0].save())
-#     logging.info("END CHECK OF USER IN DB AND CLOSE BROWSER")
-#
-
-
-# def check_user_sync(django_db_blocker):
-#     """Асинхронная версия фикстуры"""
-#
-#     logging.info(f"{__name__}")
-#     def check_user_async():
-#         with django_db_blocker.unblock():
-#             # Удаляем всех пользователей
-#             User.objects.all().delete()
-#             assert User.objects.count() == 0
-#             # logging.info("COUNT USER VALUES: %s"% str(type(count_list)))
-#             logging.info(f"GOT LIST OF USERS")
-            # count = len(list(count_list))
-            # logging.info(f"Total users in DB: {count}")
-
-            # # Вариант 2: Поиск конкретного пользователя
-            # user = User.objects.filter(username="Sergey").first()
-            # assert user is not None, "User 'Sergey' not found in database"
-            # logging.info(f"User found: {user.username}")
-            # # logging.info(f"COUNT USER: {count}, {count == 1}")
-            # assert count == 1
-            # user.delete()
-            # user.save()
-            # logging.info("END CHECK OF USER IN DB AND CLOSE BROWSER")
-
-    # return check_user_async
 @pytest.fixture
 def check_user_sync(django_db_blocker):
     """Асинхронная версия фикстуры"""
--- __tests__/playwrigh/test_register_to_the_app.py
+++ __tests__/playwrigh/test_register_to_the_app.py
@@ -7,68 +7,78 @@ import re
 import os
 import pytest
 from django.contrib.auth.models import User
-from asgiref.sync import sync_to_async, async_to_sync
-from dotenv import load_dotenv
+# from asgiref.sync import sync_to_async
+# from dotenv import load_dotenv
 # from playwright.async_api import sync_playwright
-from playwright.sync_api import Playwright, sync_playwright, expect
-from __tests__.__fixtures__.playwrigth_fixture import browser, check_user_sync
+# from playwright.sync_api import Playwright, sync_playwright, expect
+from playwright.async_api import async_playwright, Playwright, expect
+from __tests__.__fixtures__.playwrigth_fixture import abrowser #, check_user_sync
 from logs import configure_logging
 
-load_dotenv()
+# load_dotenv()
 log = logging.getLogger(__name__)
 configure_logging(logging.INFO)
 
 
-# @pytest.mark.asyncio
-
+# @pytest.mark.django_db(transaction=True)
+@pytest.fixture
+def cleaning_db(django_db_blocker):
+    """Фикстура очистки базы данных"""
+    with django_db_blocker.unblock():
+        log.info("FIXTURE CLEANING DB")
+        User.objects.all().delete()
+        log.info("FIXTURE DELETING ALL USERS")
+        return True
+    
 
 @pytest.mark.user_page
-@pytest.mark.django_db(transaction=True)
-def test_register_form_valid(browser):
-    page = browser.new_page()
-    log.info(f"OPEN: {__name__}")
-    try:
-        page.goto(f"http://{os.getenv('POSTGRES_HOST')}:8000/users/register/")
-        log.info("GOT REGISTER PAGE")
-        page.wait_for_load_state("domcontentloaded")
-        log.info("GOT DIV" )
-        expect(page).to_have_title(re.compile(r"Регистрация.*"), timeout=3000)
-        log.info("GOT TITLE")
-        """CHECK REGISTER FORM"""
-        page.fill("input[name='username']", "Sergey")
-        page.fill("input[name='email']", "sergey@gmail.com")
-        page.fill("input[name='password']", "123456789")
-        page.fill("input[name='confirm_password']", "123456789")
-        log.info("INSERT DATA IN REGISTER TORM")
-        page.keyboard.down("Enter")
-        log.info("PRESS BY ENTER")
-        """CHECK REDIRECT TO LOGIN PAGE"""
-        page.wait_for_load_state("domcontentloaded")
-        log.info("LOGIN PAGE WAS LOADED")
-        expect(page).to_have_title(re.compile(r"Войдите в"))
-        log.info("CHECK THE TITLE OF LOGIN PAGE")
-        User.objects.all().delete()
-        log.info("DELETE USERS")
-        assert User.objects.all().count() == 0
-        log.info("CHECK ALL USERS WAS DELETED")
-    except (Exception, AssertionError) as e:
-        log.error("TEST ERROR", e)
-    finally:
-        # Закрываем страницу и браузер
-        page.close()
-        log.info(f"CLOSED: {__name__}")
-        
-# @pytest.mark.django_db(transaction=True)
-# @pytest.mark.asyncio
-# async def test_clear_users_with_mark():
-#     await sync_to_async(User.objects.all().delete)()
-#     assert await sync_to_async(User.objects.count)() == 0
-#
-# @pytest.mark.user_page
-# @pytest.mark.asyncio
-# async def test_main(check_user_sync):
-#     await test_clear_users_with_mark()
-#     # test_register_form_valid()
-#     await async_to_sync(check_user_sync)()
-#     # asyncio.all_tasks()
+@pytest.mark.asyncio
+async def test_register_form_valid(cleaning_db):
+    log.info("FIXTURE START BROWSER")
+    async with async_playwright() as playwright:
+        log.info("FIXTURE RECEIVED CHROMIUM")
+        chromium = playwright.chromium
+        log.info("FIXTURE RECEIVED CHROMIUM")
+        abrowser = await chromium.launch()
+        # log.info("FIXTURE RECEIVED BROWSER")
+        # log.info("TEST REGISTER PAGE")
+        context = await abrowser.new_context()
+        # /log.info("RECEIVED CONTEXT")
+        page = await context.new_page()
+        log.info("RECEIVED NEW PAGE")
 
+        try:
+            pass
+            url = "http://127.0.0.1:8000/users/register/"
+            await page.goto(url)
+            log.info("GOT REGISTER PAGE")
+            await page.wait_for_load_state("domcontentloaded")
+            log.info("GOT DIV" )
+            await expect(page).to_have_title(re.compile(r"Регистрация.*"), timeout=3000)
+            log.info("GOT TITLE")
+            """CHECK REGISTER FORM"""
+            await page.fill("input[name='username']", "Sergey")
+            await page.fill("input[name='email']", "sergey@gmail.com")
+            await page.fill("input[name='password']", "123456789")
+            await page.fill("input[name='confirm_password']", "123456789")
+            log.info("INSERT DATA IN REGISTER TORM")
+            await page.keyboard.down("Enter")
+            log.info("PRESS BY ENTER")
+            """CHECK REDIRECT TO LOGIN PAGE"""
+            await page.wait_for_load_state("domcontentloaded")
+            log.info("LOGIN PAGE WAS LOADED")
+            await expect(page).to_have_title(re.compile(r"Войдите в"))
+            log.info("CHECK THE TITLE OF LOGIN PAGE")
+            # User.objects.all().delete()
+            # log.info("DELETE USERS")
+            # assert User.objects.all().count() == 0
+            log.info("CHECK ALL USERS WAS DELETED")
+        except (Exception, AssertionError) as e:
+            log.info("TEST ERROR", e)
+        finally:
+            pass
+            # Закрываем страницу и браузер
+            await context.close()
+            log.info("CONTEXT CLOSED")
+            await page.close()
+            log.info("PAGE CLOSED")
