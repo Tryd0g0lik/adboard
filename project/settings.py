81b7eab82274571a630a30ce97cf42eeae501416
Tryd0g0lik <work80@mail.ru>
2025-06-16 15:37:25 +0700

    [dev]: settings.py was corrected

--- project/settings.py
+++ project/settings.py
@@ -199,9 +199,8 @@ CORS_ALLOWED_ORIGINS = [
 # https://github.com/adamchainz/django-cors-headers?tab=readme-ov-file#csrf-integration
 # https://docs.djangoproject.com/en/5.2/ref/settings/#std-setting-CSRF_TRUSTED_ORIGINS
 CSRF_TRUSTED_ORIGINS = [
-    "http://83.166.245.209"
+    "http://83.166.245.209",
     "http://127.0.0.1:8000",
-    "http://80.78.242.128"
 ]
 # разрешить cookie в HTTP запросах
 CORS_ALLOW_CREDENTIALS = True
