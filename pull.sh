2f70cbef3b305d6e9889178237c3162683bcbc55
Tryd0g0lik <work80@mail.ru>
2025-06-16 11:05:28 +0700

    [dev]: *.sh, 'workflows/main.yml' was  updated

--- pull.sh
+++ pull.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 cd /home/denis/adboard || exit 1
 git pull --ff-only origin dev  || exit 1
-sudo systemctl restart gunicorn  || exit 1
+sudo docker compose down || exit 1
+sudo docker rmi $(sudo docker images -q) || exit 1
+sudo docker compose -f ./docker-compos* up -d || exit 1
 echo "Deployment completed successfully"
 
 
