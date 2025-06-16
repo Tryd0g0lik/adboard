b0e5f73debe0979c8e732d23c4954297c46364c1
Tryd0g0lik <work80@mail.ru>
2025-06-16 10:42:34 +0700

    [dev]: *.sh  was  updated

--- pull.sh
+++ pull.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 cd /home/denis/adboard || exit 1
-git pull --ff-only origin dev  || exit 2
-# git commit -m "Update" || exit 1
-sudo systemctl restart gunicorn  || exit 5
-# sudo systemctl restart nginx  || exit 4
+git pull --ff-only origin dev  || exit 1
+sudo systemctl restart gunicorn  || exit 1
 echo "Deployment completed successfully"
-#git fetch origin tests
-# git reset --hard origin/test
+
 
