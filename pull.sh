d76c5e38149bb32477a14b2e2c095529e8a5201b
Denis <63048913+Tryd0g0lik@users.noreply.github.com>
2025-06-17 07:27:09 +0700

    Update pull.sh

--- pull.sh
+++ pull.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 cd /home/denis/adboard || exit 1
-git pull --ff-only origin dev  || exit 2
-# git commit -m "Update" || exit 1
-sudo systemctl restart gunicorn  || exit 5
-sudo systemctl restart nginx  || exit 4
-echo "Deployment completed successfully"
-#git fetch origin tests
-# git reset --hard origin/test
+git pull --ff-only origin cicd  || exit 1
+# sudo docker compose down || exit 1
+if [ "$(sudo docker ps -aq)" ]; then
+    echo "Found running containers, stopping them..."
+    sudo docker compose down || exit 1
+    sudo docker builder prune -af  || exit 1
+else
+    echo "No running containers found"
+fi
 
+# sudo docker rmi $(sudo docker images -q) || exit 1
+if [ "$(sudo docker images -q)" ]; then
+    echo "Found docker images, removing them..."
+    sudo docker rmi $(sudo docker images -q) || exit 1
+else
+    echo "No docker images found"
+fi
+
+
+#sudo docker compose -f ./docker-compose.yml build --no-cache  # Пересборка всех сервисов
+sudo docker compose -f ./docker-compose.yml up -d
+
+echo "Deployment completed successfully"
