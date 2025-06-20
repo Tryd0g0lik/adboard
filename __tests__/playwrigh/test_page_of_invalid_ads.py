8c41a6ed2236351a9d5bb09088dc907694803bbd
Tryd0g0lik <work80@mail.ru>
2025-06-20 18:38:06 +0700

    [tests]: README file was added to the '__tests__' directory

--- __tests__/playwrigh/test_page_of_invalid_ads.py
+++ __tests__/playwrigh/test_page_of_invalid_ads.py
@@ -27,7 +27,8 @@ valid_ad = {
  самолетов МС-21, оставшиеся 92 воздушных судна поступят в парк авиакомпании только к концу 2032 года. Компания \
  при этом не планирует",
 }
-set = set()
+set_title = set()
+set_description = set()
 
 
 @pytest.mark.ads
@@ -38,7 +39,7 @@ set = set()
     [
         (
             "ads_one",
-            "В Совете по инвестициям заявили",
+            "1234567890",
             401,
         ),
         (
@@ -94,38 +95,30 @@ async def test_title_field_of_ads_invalid(
                 valid_ad["description"],
             )
             log.info("USER HAS BEEN WRITING FIELDS OF HIS FORM FOR NEW AD")
-            async with page.expect_response(
-                url + "/api/v1/ads/index/"
-            ) as response_info:
-                log.info("REQUEST")
-                await page.get_by_role("button", name="Submit").click()
-                await page.screenshot(
-                    path="__tests__/screen/ads/%s_test_page_of_invalid_ads.png" % user
-                )
+            await page.get_by_role("button", name="Submit").click()
+            await page.screenshot(
+                path="__tests__/screen/ads/%s_test_page_of_invalid_ads.png" % user
+            )
 
-                log.info(
-                    "USER HAS BEEN CLICKED ON BUTTON FOR CREATING NEW REQUEST TO SERVER "
-                )
-                locator = page.locator('p[class="invalid"]')
-                log.info("LOCATOR: %s" % locator)
-                # Below line is to get error message, and for braiked this cycle - it's specially.
-                await expect(locator).to_have_text("Value is not valid")
-            # log.info("FIELDS FILLED WITH VALID DATA")
-            # response = await response_info.value
-            # log.info("RESPONSE: %s" % response.status)
-            # assert response.status == answer
+            log.info(
+                "USER HAS BEEN CLICKED ON BUTTON FOR CREATING NEW REQUEST TO SERVER "
+            )
+
+            locator = page.locator('p[class="invalid"]')
+            log.info("LOCATOR: %s" % locator)
+            await expect(locator).to_contain_text(re.compile(r".*valid.*"))
 
         except Exception as ex:
             log.error("ADS PAGE ERROR %s" % ex)
-            set.add(user)
+            set_title.add(user)
         finally:
             log.info("TEST WAS CLOSED")
             await page.close()
             log.info("PAGE CLOSED")
             await sync_to_async(delete_one_user)(user)
             log.info("USER AND ADS HAS BEEN DELETED.")
-            log.info("HAS BEEN ERROR TO THE NEXT USERS: %s" % set)
-            set.clear()
+            log.info("TITLE: HAS BEEN ERROR TO THE NEXT USERS: %s" % set_title)
+
     @pytest.mark.ads
     @pytest.mark.ad_create
     @pytest.mark.ad_get
@@ -176,46 +169,41 @@ async def test_title_field_of_ads_invalid(
                 log.info("USER LOGIN '%s' IS STARTING" % user)
                 url = f"http://{os.getenv('POSTGRES_HOST')}:8000"
                 page = await one_of_ads(page, url, expect, user)
-                await page.wait_for_load_state("domcontentloaded",
-                                               timeout=3000
-                                               )
+                await page.wait_for_load_state("domcontentloaded", timeout=3000)
                 log.info("USER TO THE MAIN PAGE")
                 await page.goto(url + "/user/ads/")
-                await  page.await_for_load_state(
-                    "domcontentloaded", timeout=3000)
+                await page.await_for_load_state("domcontentloaded", timeout=3000)
                 locator = page.locator("h3", has_text="Объявление")
                 await expect(locator).to_contain_text("Объявление")
                 log.info("USER WAS GOING TO PAGE OF HIS ADS")
                 await page.fill("input[type='text']", valid_ad["title"])
-                await page.fill("textarea[name='description']",
-                                description,)
+                await page.fill(
+                    "textarea[name='description']",
+                    description,
+                )
                 log.info("USER HAS BEEN WRITING FIELDS OF HIS FORM FOR NEW AD")
-                async with page.except_response(url + "/api/v1/ads/index/"):
-                    log.info("REQUEST")
-                    await page.get_by_role("button", name="Submit").click()
-                    await page.screenshot(
-                        path="__tests__/screen/ads/%s_test_page_of_invalid_description_ads.png" % user
-                    )
-                    log.info(
-                        "USER HAS BEEN CLICKED ON BUTTON FOR CREATING NEW REQUEST TO SERVER "
-                    )
-                    locator = page.locator('p[class="invalid"]')
-                    log.info("LOCATOR: %s" % locator)
-                    # Below line is to get error message, and for braiked this cycle - it's specially.
-                    await expect(locator).to_have_text("Value is not valid")
-                # log.info("FIELDS FILLED WITH VALID DATA")
-                # response = await response_info.value
-                # log.info("RESPONSE: %s" % response.status)
-                # assert response.status == answer
+                await page.get_by_role("button", name="Submit").click()
+                await page.screenshot(
+                    path="__tests__/screen/ads/%s_test_page_of_invalid_description_ads.png"
+                    % user
+                )
+                log.info(
+                    "USER HAS BEEN CLICKED ON BUTTON FOR CREATING NEW REQUEST TO SERVER "
+                )
+                locator = page.locator('p[class="invalid"]')
+                log.info("LOCATOR: %s" % locator)
+                await expect(locator).to_contain_text("Value is not valid")
 
             except Exception as ex:
                 log.error("ADS PAGE ERROR %s" % ex)
-                set.add(user)
+                set_description.add(user)
             finally:
                 log.info("TEST WAS CLOSED")
                 await page.close()
                 log.info("PAGE CLOSED")
                 await sync_to_async(delete_one_user)(user)
                 log.info("USER AND ADS HAS BEEN DELETED.")
-                log.info("HAS BEEN ERROR TO THE NEXT USERS: %s" % set)
-                set.clear()
+                log.info(
+                    "DESCRIPTION: HAS BEEN ERROR TO THE NEXT USERS: %s"
+                    % set_description
+                )
