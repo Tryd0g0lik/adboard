cc4d3d62602600934e431f95d7aafcfb7ab795b5
Tryd0g0lik <work80@mail.ru>
2025-06-27 11:58:11 +0700

    [dav]: update

--- project/settings.py
+++ project/settings.py
@@ -169,9 +169,6 @@ STATICFILES_DIRS = [
     # os.path.join(BASE_DIR,  "weather/static")
 ]
 STATIC_ROOT = os.path.join(BASE_DIR,  "collectstatic/")
-# STATIC_ROOT = "/www/src/static/"
-# STATIC_ROOT = "/www/src/collectstatic"
-# STATIC_URL = os.environ.get("STATIC_URL", "/static/")
 STATIC_URL = "/static/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR,  "media/")
